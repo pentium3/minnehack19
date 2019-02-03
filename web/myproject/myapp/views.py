99c4942f071ae03ebddb1d17da636bd27aa19148
HeatherJiaZG <yjia42@wisc.edu>
2019-02-03 07:13:02 -0600

    'ADDtextbox'

--- web/myproject/myapp/views.py
+++ web/myproject/myapp/views.py
@@ -9,36 +9,55 @@ from myproject.myapp.forms import DocumentForm
 from myproject.settings import MEDIA_ROOT
 from myproject.myapp.classify import classify
 
+# def list(request):
+#     print('hahahahahahahaha')
+#     filestr=''
+#     # Handle file upload
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             newdoc = Document(docfile=request.FILES['docfile'])
+#             newdoc.save()
+#             #print newdoc.docfile.url, newdoc.docfile.name
+#             filestr=MEDIA_ROOT + '/' + newdoc.docfile.name
+#             res=233333
+#             res=classify(filestr)
+
+#             # Redirect to the document list after POST
+#             # Render list page with the documents and the form
+#             return render(
+#                 request,
+#                 'list.html',
+#                 {'form': form, 'newdoc': filestr, 'result': res, }
+#             )
+#             return HttpResponseRedirect(reverse('list'))
+#     else:
+#         form = DocumentForm()  # A empty, unbound form
+
+#     # Load documents for the list page
+#     #documents = Document.objects.all()
+
+#     # Render list page with the documents and the form
+#     return render(
+#         request,
+#         'list.html',
+#         {'form': form, 'newdoc': filestr, }
+#     )
+
+
+ 
 def list(request):
-    filestr=''
-    # Handle file upload
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile=request.FILES['docfile'])
-            newdoc.save()
-            #print newdoc.docfile.url, newdoc.docfile.name
-            filestr=MEDIA_ROOT + '/' + newdoc.docfile.name
-            res=233333
-            res=classify(filestr)
-
-            # Redirect to the document list after POST
-            # Render list page with the documents and the form
-            return render(
-                request,
-                'list.html',
-                {'form': form, 'newdoc': filestr, 'result': res, }
-            )
-            return HttpResponseRedirect(reverse('list'))
-    else:
-        form = DocumentForm()  # A empty, unbound form
-
-    # Load documents for the list page
-    #documents = Document.objects.all()
-
-    # Render list page with the documents and the form
+    zipcode = request.GET.get('zipcode','')
+    plant = request.GET.get('choices-single-defaul','')
+    results = []
+    if zipcode:
+        results += str(zipcode)
+    if plant:
+        results += str(plant)
+    print(zipcode)
+    print(plant)
     return render(
         request,
         'list.html',
-        {'form': form, 'newdoc': filestr, }
+        {'results': results, }
     )
